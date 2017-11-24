8daa99a2886786de9bfcfa049c7695355876b93f
Pos Unipe <posunipe@jovennans-mac.local>
2017-11-24 19:17:33 -0300

    Initial Commit

---
+++ HoOGas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HoOGas
+//
+//  Created by Pos Unipe on 24/11/2017.
+//  Copyright © 2017 Pos Unipe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
