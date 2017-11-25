9745f50bb93c7c1f407b9b26ef624732c742d467
Jovennan <jovennan@gmail.com>
2017-11-24 23:04:17 -0300

    Modelagem de telas concluido

---
+++ HoOGas/Controller/LoginController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginController.swift
+//  HoOGas
+//
+//  Created by Pos Unipe on 24/11/2017.
+//  Copyright © 2017 Pos Unipe. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    
+    @IBOutlet var login: UITextField!
+    @IBOutlet var senha: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
